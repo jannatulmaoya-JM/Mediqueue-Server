596d9a7da70677d99dc9c9be00533ea9997d0742
maoya12345 <jannatulmaoya456@gmail.com>
2026-06-16 18:35:08 +0600

    complite the server side

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 dotenv.config();
-
 const uri = process.env.DB_URI;
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -179,7 +177,6 @@ async function run() {
       }
     });
 
-   
     app.patch("/bookings/:id", async (req, res) => {
       try {
         const { id } = req.params;
